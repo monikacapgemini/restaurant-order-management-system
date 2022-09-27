af480f82a3353f4bddfe322a9d8ff4a205365b01
pobbatti vijaya lakshmi <honeyvijji643@gmail.com>
2022-09-28 00:07:15 +0530

    Add project files.

---
+++ src/app/global/drinks-icon/drinks-icon.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DrinksIconComponent } from './drinks-icon.component';
+
+describe('DrinksIconComponent', () => {
+  let component: DrinksIconComponent;
+  let fixture: ComponentFixture<DrinksIconComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DrinksIconComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DrinksIconComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/home/header-image/header-image.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FetchHeaderImageService } from '../../services/fetch-header-image.service';
+
+@Component({
+  selector: 'app-header-image',
+  templateUrl: './header-image.component.html',
+  styleUrls: ['./header-image.component.css'],
+})
+export class HeaderImageComponent implements OnInit {
+
+  isFetchingImage: boolean = false;
+  imageUrl: any;
+
+  constructor(private fetchImgService: FetchHeaderImageService) {}
+
+  ngOnInit(): void {
+    this.isFetchingImage = true;
+
+    this.fetchImgService.fetchImage().subscribe(
+      (response) => {
+        //console.log(response);
+      },
+      (error) => {
+        this.imageUrl = error.url;
+        this.isFetchingImage = false;
+      }
+    );
+  }
+}
